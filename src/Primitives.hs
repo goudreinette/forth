04555572d41e40fa63427189c4912fbcbfd90ada
reinvdwoerd <reinvanderwoerd@me.com>
2017-06-05 16:09:20 +0200

    more primitives

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -3,12 +3,21 @@ module Primitives where
 import           Types
 
 primitiveWords =
-  [("+", numOp (+)),
-   ("-", numOp (-)),
+  [("+", numBinOp (+)),
+   ("-", numBinOp (-)),
+   ("*", numBinOp (*)),
+   ("/", numBinOp quot),
+   ("max", numBinOp max),
+   ("min", numBinOp min),
+   ("negate", numOp negate),
+   ("abs", numOp abs),
    (".", pop)]
 
-
 numOp f = do
+  (Number x) <- pop
+  push (Number (f x))
+
+numBinOp f = do
   (Number x) <- pop
   (Number y) <- pop
   push (Number (f x y))
